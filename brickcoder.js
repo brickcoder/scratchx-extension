898fe16045c50da732618e7aeb05eb2a148e1b89
Philipp Henkel <henkel42@gmail.com>
2016-06-14 23:45:38 +0200

    Add parameterized blocks

--- brickcoder.js
+++ brickcoder.js
@@ -18,13 +18,30 @@
         });
     };
 
+    ext.sendRawCommand = function(location, callback) {
+        $.ajax({
+              url: 'http://localhost:9555/test'
+        });
+    };
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name
             [' ', 'my first block', 'my_first_block'],
             [' ', 'call http', 'call_http'],
-        ]
+            [' ', 'turn all off', 'call_http'],
+            [' ', 'turn %m.device on', 'call_http', 'CH1 RED'],
+            [' ', 'turn %m.device off', 'call_http', 'CH1 RED'],
+            [' ', 'set direction of %m.device to %m.direction', 'call_http', 'CH1 RED','forward'],
+            [' ', 'set power of %m.device to %m.power', 'call_http', 'CH1 RED','7'],
+            [' ', 'send raw command %n', 'sendRawCommand', 407],
+        ],
+        menus: {
+            device: ['CH1 RED', 'CH1 BLUE', 'CH2 RED', 'CH2 BLUE', 'CH3 RED', 'CH3 BLUE', 'CH4 RED', 'CH4 BLUE'],
+            power: ['0', '1', '2', '3', '4', '5', '6', '7'],
+            direction: ['forward', 'backward'],
+        }
     };
 
     // Register the extension
